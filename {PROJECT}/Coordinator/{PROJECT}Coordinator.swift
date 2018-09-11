de77fa7355a50fac1c4fa754586f469872d8ef74
Antonio Junior <50percentgray@gmail.com>
2018-09-11 00:21:46 -0300

    Add files via upload

---
+++ {PROJECT}/Coordinator/{PROJECT}Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  {PROJECT}.swift
+//  {ORGANIZATION}
+//
+//  Created by {AUTHOR} on {TODAY}.
+//  Copyright © {YEAR} {ORGANIZATION}. All rights reserved.
+//
+
+import Foundation
+
+final class {PROJECT}Coordinator {
+    
+}
---
+++ {PROJECT}/View/{PROJECT}ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  {PROJECT}.swift
+//  {ORGANIZATION}
+//
+//  Created by {AUTHOR} on {TODAY}.
+//  Copyright © {YEAR} {ORGANIZATION}. All rights reserved.
+//
+
+import UIKit
+
+final class {PROJECT}ViewController: UIViewController {
+    
+}
